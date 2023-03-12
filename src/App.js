d3cdc1c5affd4ab46a4f567ec986a17e2268b473
Lee-Dillon <lee.dillon1@outlook.com>
2023-03-12 13:50:09 +0000

    eod 1303

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/pages/Home';
-import Discover from './components/pages/Discover';
-import Search from './components/pages/Search';
+// import Discover from './components/pages/Discover';
+// import Search from './components/pages/Search';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,8 +17,6 @@ function App() {
           {/* Define routes using the Route component to render different page components at different paths */}
           {/* Define a default route that will render the Home component */}
           <Route path="/" element={<Home />} />
-          <Route path="discover" element={<Discover />} />
-          <Route path="search/*" element={<Search />} />
         </Routes>
       </div>
     </Router>
